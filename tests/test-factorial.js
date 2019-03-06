1406d49e18506f4845ee92aba8d77501ead0d2b7
Domenico Pontari <fairsayan@gmail.com>
2019-03-06 10:10:38 +0100

    no message

--- tests/test-factorial.js
+++ tests/test-factorial.js
@@ -1,15 +1,6 @@
-describe('factorial function', function() {
-  it('of 3 should be 6', function() {
-    document.body.innerHTML = __html__['public/index.html']
-    document.body.onload = () => {
-      expect(factorial(3)).toBe(6)
-    }
-  })
-})
-
 describe('element #first', function () {
   it('is Domenico', function () {
-    let first = document.getElementById('first').innerHTML
-    expect(first).toBe('Domenico')
+    document.body.innerHTML = __html__['public/index.html']
+    expect(first.innerHTML).toBe('Domenico')    
   })
 })
