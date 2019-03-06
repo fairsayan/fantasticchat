1c9d4a7d6b922a2f987c2779978f11386cd3d1a4
Domenico Pontari <fairsayan@gmail.com>
2019-03-06 12:36:39 +0100

    new test

--- karma.conf.js
+++ karma.conf.js
@@ -16,6 +16,7 @@ module.exports = function(config) {
     // list of files / patterns to load in the browser
     files: [
       'public/index.html',
+      'public/factorial.js',
       'tests/**/*.js'
     ],
 
--- tests/test-factorial.js
+++ tests/test-factorial.js
@@ -1,6 +1,13 @@
 describe('element #first', function () {
   it('is Domenico', function () {
     document.body.innerHTML = __html__['public/index.html']
+    var first = document.getElementById('first')
     expect(first.innerHTML).toBe('Domenico')    
+  })
+})
+
+describe('funzione fattoriale', function() {
+  it('il fattoriale di 3 è 6', function () {
+    expect(factorial(3)).toBe(6)
   })
 })
